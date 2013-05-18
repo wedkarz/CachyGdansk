55778682f4623167f98cefeff4c1d490ea273d5a
Artur Rybak <artur.rybak@gmail.com>
2013-05-18 10:27:53 +0200

    Initial Commit

---
+++ CachyGdansk/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  CachyGdansk
+//
+//  Created by Artur Rybak on 5/18/13.
+//  Copyright (c) 2013 Kainos Software Polska. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CachyGdansk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CachyGdansk
+//
+//  Created by Artur Rybak on 5/18/13.
+//  Copyright (c) 2013 Kainos Software Polska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
